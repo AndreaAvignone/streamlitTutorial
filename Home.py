44bb5117d697bb0104e0a9accc629c2d9d26e3ff
Andrea Avignone <andrea.avignone@gmail.com>
2023-05-01 22:35:09 +0200

    init

--- Home.py
+++ Home.py
@@ -3,8 +3,9 @@ import numpy as np
 import pandas as pd
 
 st.set_page_config(
-    page_title="La mia App",
+    page_title="Business Analytics",
     layout="wide",
+    page_icon="📈",
     initial_sidebar_state="expanded",
     menu_items={
         'Get Help': 'https://dbdmg.polito.it/',
@@ -12,3 +13,10 @@ st.set_page_config(
         'About': "# Corso di *Basi di Dati*"
     }
 )
+col1,col2=st.columns([3,2])
+with col1:
+    st.title(":red[Live Coding] Session")
+    st.markdown("## Corso di :blue[**Basi di Dati**]")
+    st.markdown("#### 🧑‍💻️ Creazione di una dashboard")
+with col2:
+    st.image("images/polito_white.png")
