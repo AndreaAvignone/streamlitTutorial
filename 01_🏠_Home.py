78670175f5969706a84d35dcc960ed31b6509a41
Andrea Avignone <andrea.avignone@gmail.com>
2023-05-07 19:02:50 +0200

    Create 01_🏠_Home.py

---
+++ 01_🏠_Home.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from utils.utils import *
+
+if __name__ == "__main__":
+    st.set_page_config(
+        page_title="Business Analytics",
+        layout="wide",
+        page_icon="🗂",
+        initial_sidebar_state="expanded",
+        menu_items={
+            'Get Help': 'https://dbdmg.polito.it/',
+            'Report a bug': "https://dbdmg.polito.it/",
+            'About': "# Corso di *Basi di Dati*"
+        }
+    )
+
+
+    col1,col2=st.columns([3,2])
+    with col1:
+        st.title(":red[Live Coding] Session")
+        st.markdown("## Corso di :blue[Basi di Dati]")
+        st.markdown("#### 🧑‍💻️ Creazione di una dashboard")
+    with col2:
+        st.image("images/polito_white.png")
+        
+    if "connection" not in st.session_state.keys():
+        st.session_state["connection"]=False
+
+    check_connection()
