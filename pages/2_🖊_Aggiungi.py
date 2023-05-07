c0355cb1405e9102f14ea70ec17133519895257d
Andrea Avignone <andrea.avignone@gmail.com>
2023-05-07 19:03:41 +0200

    add new product page

---
+++ pages/2_🖊_Aggiungi.py
@@ -0,0 +1,75 @@
+import streamlit as st
+from utils.utils import *
+
+#query base per recuperare tutti i valori distinti che un attributo può avere
+#utilizzato per creare la lista di opzioni tra cui scegliere
+def get_list(attributo):
+    query=f"SELECT DISTINCT {attributo} FROM products"
+    result=execute_query(st.session_state["connection"],query)
+    result_list=[]
+    for row in result.mappings():
+        result_list.append(row[attributo])
+    return result_list
+
+def get_info():
+    return get_list("productLine"),get_list("productScale"),get_list("productVendor")
+
+#controlla se tutti i campi testuali sono stati riempiti
+def check_info(prod_dict):
+    for value in prod_dict.values():
+        if value=='':
+            return False
+    return True
+
+#inserisce il nuovo prodotto
+def insert(prod_dict):
+    if check_info(prod_dict):
+        attributi=", ".join(prod_dict.keys())
+        valori=tuple(prod_dict.values())
+        query=f"INSERT INTO products ({attributi}) VALUES {valori};"
+        #try-except per verificare che l'operazione MySQL abbia avuto successo, generare un errore altrimenti
+        try:
+            execute_query(st.session_state["connection"],query)
+            st.session_state["connection"].commit()
+        except Exception as e:
+            st.error(e)
+            return False
+        return True
+    else:
+        return False
+
+def create_form():
+    with st.form("Nuovo Prodotto"):
+        st.header(":blue[Aggiungi prodotto:]")
+
+        #parametri
+        categorie,scale,venditori=get_info()
+        code=st.text_input("Codice prodotto",placeholder="S**_****")
+        nome=st.text_input("Nome prodotto",placeholder="Inserisci il nome del prodotto")
+        categoria=st.selectbox("Categoria",categorie)
+        scala=st.selectbox("Scala prodotto",scale)
+        venditore=st.selectbox("Venditore",venditori)
+        descrizione=st.text_area("Descrizione",placeholder="Inserisci la descrizione del prodotto")
+        qta=st.slider("Quantità",0,10000)
+        prezzo=st.number_input("Prezzo",1.00)
+        msrp=st.number_input("MSRP")
+
+        #dizionario finale con tutti i parametri
+        insert_dict= {"productCode":code, "productName":nome,"productLine":categoria,"productScale":scala,"productVendor":venditore,"productDescription":descrizione,"quantityInStock":qta,"buyPrice":prezzo,"MSRP":msrp}
+        
+        #tasto submit fondamentale nel form
+        submitted =st.form_submit_button("Submit",type='primary')
+    
+    if submitted:
+        #verificare che l'inserimento sia andato a buon fine oppure no
+        if insert(insert_dict):
+            st.success("✅ Hai inserito questo prodotto: ")
+            st.write(insert_dict)
+        else:
+            st.error("⚠️ Impossibile aggiungere il prodotto.")
+ 
+
+if __name__ == "__main__":
+    st.title("🖊 Modifica")
+    if check_connection():
+        create_form()
