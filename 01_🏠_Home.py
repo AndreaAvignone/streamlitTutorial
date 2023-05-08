80b1306f98fa8665fc63a848c24a571d3f16deba
Andrea Avignone <64846335+AndreaAvignone@users.noreply.github.com>
2023-05-08 16:04:23 +0200

    Revert "Live coding complete"

--- 01_🏠_Home.py
+++ 01_🏠_Home.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from utils.utils import *
-import pymysql,cryptography
 
 if __name__ == "__main__":
     st.set_page_config(
--- utils/utils.py
+++ utils/utils.py
@@ -2,3 +2,4 @@ import streamlit as st
 from sqlalchemy import create_engine,text
 
 """Raccoglie le principali funzioni condivise dalle varie pagine"""
+
